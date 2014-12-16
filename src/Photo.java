664dbf76194b4f63dd1f7b5f16ad7354d34b8a78
Weizhen Wang <ww2339@columbia.edu>
2014-12-16 11:32:47 -0500

    Add posting comments.

--- src/Photo.java
+++ src/Photo.java
@@ -3,23 +3,25 @@ import java.util.List;
 
 
 public class Photo {
+	public String url;
 	public String date;
 	public String dateFormat;
 	public String title;
 	public String content;
-	public List<String> simmilarUrls;
+	public List<String> similarUrls;
 	public List<Comment> comments;
 	
 	public Photo(String date, String title, String content, String url, List<String> urls, List<Comment> comments) {
+		this.url = url;
 		this.date = date;
 		this.dateFormat = "D/MM/YY h:mm A";
 		this.title = title;
 		this.content = content;
-		this.simmilarUrls = new LinkedList<String>();
-		this.simmilarUrls.add("https://s3.amazonaws.com/footprint.linhuang/winter.jpg");
-		this.simmilarUrls.add("https://s3.amazonaws.com/footprint.linhuang/winter1.jpg");
-		this.simmilarUrls.add("https://s3.amazonaws.com/footprint.linhuang/winter2.jpg");
-		this.simmilarUrls.add("https://s3.amazonaws.com/footprint.linhuang/winter3.jpg");
+		this.similarUrls = new LinkedList<String>();
+		this.similarUrls.add("https://s3.amazonaws.com/footprint.linhuang/winter.jpg");
+		this.similarUrls.add("https://s3.amazonaws.com/footprint.linhuang/winter1.jpg");
+		this.similarUrls.add("https://s3.amazonaws.com/footprint.linhuang/winter2.jpg");
+		this.similarUrls.add("https://s3.amazonaws.com/footprint.linhuang/winter3.jpg");
 		this.comments = comments;
 	}
 }
